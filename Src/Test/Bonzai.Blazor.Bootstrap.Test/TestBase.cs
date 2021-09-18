cce07b0331540a1ee5dd4dd33790bd10b3bd3711
Andrew Bonsall <middlebonsall03@gmail.com>
2021-09-18 19:20:24 -0400

    Added test for Column component. Minor change to TextInput component.

---
+++ Src/Test/Bonzai.Blazor.Bootstrap.Test/TestBase.cs
@@ -0,0 +1,17 @@
+﻿using Bunit;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonzai.Blazor.Bootstrap.Test
+{
+    public class TestBase<TComponent> : TestContext
+        where TComponent : ComponentBase
+    {
+        protected IRenderedComponent<TComponent> RenderComponent(RenderFragment renderFragment) =>
+            Render<TComponent>(renderFragment);
+    }
+}
