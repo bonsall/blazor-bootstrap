35e0d81dfa52a45b32d178a296276bd70d7b730a
Andrew Bonsall <middlebonsall03@gmail.com>
2021-09-28 20:09:13 -0400

    Added breadcrumb and breadcrumb item

---
+++ Src/Bonzai.Blazor.Bootstrap.Documentation/Client/Pages/Components/DocsBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Bonzai.Blazor.Bootstrap.Documentation.Client.Pages.Components
+{
+    public class DocsBase : ComponentBase
+    {
+        private int _exampleNumber;
+
+        protected int ExampleNumber
+        {
+            get
+            {
+                return ++_exampleNumber;
+            }
+        }
+    }
+}
