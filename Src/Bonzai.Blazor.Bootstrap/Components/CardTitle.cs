80267b59220e339566589305180e79d65c599bcd
Andrew Bonsall <middlebonsall03@gmail.com>
2021-10-22 15:07:35 -0400

    Added card title components

---
+++ Src/Bonzai.Blazor.Bootstrap/Components/CardTitle.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Bonzai.Blazor.Bootstrap.Components
+{
+    public class CardTitle1 : CardTitleBase
+    {
+        protected override string Element => "h1";
+    }
+    public class CardTitle2 : CardTitleBase
+    {
+        protected override string Element => "h2";
+    }
+    public class CardTitle3 : CardTitleBase
+    {
+        protected override string Element => "h3";
+    }
+    public class CardTitle4 : CardTitleBase
+    {
+        protected override string Element => "h4";
+    }
+    public class CardTitle5 : CardTitleBase
+    {
+        protected override string Element => "h5";
+    }
+    public class CardTitle6 : CardTitleBase
+    {
+        protected override string Element => "h6";
+    }
+}
---
+++ Src/Bonzai.Blazor.Bootstrap/Components/CardTitleBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonzai.Blazor.Bootstrap.Components
+{
+    public abstract class CardTitleBase : BootstrapContainerBase
+    {
+        protected abstract string Element { get; }
+
+        protected override string DefaultClass => "card-title";
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, Element);
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddAttribute(2, "class", Classes);
+            builder.AddContent(3, ChildContent);
+            builder.CloseElement();
+        }
+    }
+}
