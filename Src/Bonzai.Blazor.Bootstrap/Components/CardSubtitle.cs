aa2c9239f192dd53e257bd52c63f5203b3c8093a
Andrew Bonsall <middlebonsall03@gmail.com>
2021-10-22 15:16:39 -0400

    Added card subtitle components

---
+++ Src/Bonzai.Blazor.Bootstrap/Components/CardSubtitle.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Bonzai.Blazor.Bootstrap.Components
+{
+    public class CardSubtitle1 : CardSubtitleBase
+    {
+        protected override string Element => "h1";
+    }
+    public class CardSubtitle2 : CardSubtitleBase
+    {
+        protected override string Element => "h2";
+    }
+    public class CardSubtitle3 : CardSubtitleBase
+    {
+        protected override string Element => "h3";
+    }
+    public class CardSubtitle4 : CardSubtitleBase
+    {
+        protected override string Element => "h4";
+    }
+    public class CardSubtitle5 : CardSubtitleBase
+    {
+        protected override string Element => "h5";
+    }
+    public class CardSubtitle6 : CardSubtitleBase
+    {
+        protected override string Element => "h6";
+    }
+}
---
+++ Src/Bonzai.Blazor.Bootstrap/Components/CardSubtitleBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+using System;
+
+namespace Bonzai.Blazor.Bootstrap.Components
+{
+    public abstract class CardSubtitleBase : BootstrapContainerBase
+    {
+        protected abstract string Element { get; }
+
+        protected override string DefaultClass => "card-subtitle";
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, Element);
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddAttribute(2, "class", Classes);
+            builder.AddContent(3, ChildContent);
+            builder.CloseElement();
+        }
+    }
+}
