9775dae5f961c70aa70f7cb2d0c79b5ddc6ad671
Andrew Bonsall <middlebonsall03@gmail.com>
2021-10-23 10:37:30 -0400

    Added after render action queue to base component.

--- Src/Bonzai.Blazor.Bootstrap/BootstrapComponentBase.cs
+++ Src/Bonzai.Blazor.Bootstrap/BootstrapComponentBase.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Bonzai.Blazor.Bootstrap
 {
     public abstract class BootstrapComponentBase : ComponentBase
     {
+        private Queue<Action> _afterRenderActions = new Queue<Action>();
+        private Queue<Func<Task>> _afterRenderAsyncActions = new Queue<Func<Task>>();
+
         [Parameter(CaptureUnmatchedValues = true)]
         public IDictionary<string, object> AdditionalAttributes { get; set; }
 
@@ -23,5 +29,31 @@ namespace Bonzai.Blazor.Bootstrap
         }
 
         protected virtual string DefaultClass { get; }
+
+        protected void AfterRender(Action action)
+        {
+            _afterRenderActions.Enqueue(action);
+        }
+
+        protected void AfterRender(Func<Task> action)
+        {
+            _afterRenderAsyncActions.Enqueue(action);
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            while(_afterRenderActions.Any())
+            {
+                _afterRenderActions.Dequeue()();
+            }
+        }
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            while (_afterRenderAsyncActions.Any())
+            {
+                await _afterRenderAsyncActions.Dequeue()();
+            }
+        }
     }
 }
