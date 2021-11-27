b845f08449a501fd0ed50f72621747250a144e66
Andrew Bonsall <middlebonsall03@gmail.com>
2021-11-27 16:22:48 -0500

    Begin navigation menu.

---
+++ Src/Bonzai.Blazor.Bootstrap.Documentation/Client/ViewModels/RouteModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bonzai.Blazor.Bootstrap.Documentation.Client.ViewModels
+{
+    public class RouteModel
+    {
+        public string Route { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public List<RouteModel> ChildRoutes { get; set; }
+    }
+}
